9193134cfd57d8fa0e9b09abcb411ec79ecfc9f3
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-01-25 17:05:14 +0400

    portfolio section added

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,26 @@ window.addEventListener("scroll",function(){
 })
 
 // Naviqation scrool effekti---end---
+
+// ----GÖRÜLEN İŞLER BOLMESİ POPUP CARD---START----
+const porfolioModals = document.querySelectorAll(".portfolioModel");
+const imgCards = document.querySelectorAll(".imgCard");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn")
+
+var porfolioModal = function (modalClick) {
+    porfolioModals[modalClick].classList.add("active");
+}
+imgCards.forEach((imgCard,i)=>{
+    imgCard.addEventListener("click", ()=>{
+        porfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn)=> {
+    portfolioCloseBtn.addEventListener("click", ()=>{
+        porfolioModals.forEach((porfolioModalView) => {
+            porfolioModalView.classList.remove("active");
+        });
+    });
+});
+// ----GÖRÜLEN İŞLER BOLMESİ POPUP CARD---END----
