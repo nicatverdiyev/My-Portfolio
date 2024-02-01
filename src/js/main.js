806a57f71fa5aba55fe99fbd3ed4f93cc19d220c
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-02-01 19:02:59 +0400

    dark mode color added to  variables.scss

--- src/js/main.js
+++ src/js/main.js
@@ -56,4 +56,12 @@ themeBtn.addEventListener("click", ()=> {
 const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
 const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
 
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if (savedTheme) {
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
 // ----DARK--MODE--BUTTON---END----
