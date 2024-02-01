c5cea175273839bccbc9bbe196044e8b0f66b7f0
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-02-01 19:33:18 +0400

    proje colors changed

--- src/js/main.js
+++ src/js/main.js
@@ -45,21 +45,44 @@ scrollTopBtn.addEventListener("click",()=> {
 
 // ----DARK--MODE--BUTTON---START----
 
+// const themeBtn = document.querySelector(".theme-btn");
+// themeBtn.addEventListener("click", ()=> {
+//     document.body.classList.toggle("dark-theme");
+//     themeBtn.classList.toggle("sun");
+//     localStorage.setItem("saved-theme", getCurrentTheme());
+//     localStorage.setItem("saved-icon", getCurrentIcon());
+// });
+
+// const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+// const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+// const savedTheme = localStorage.getItem("saved-theme");
+// const savedIcon = localStorage.getItem("saved-icon");
+
+// if (savedTheme) {
+//     document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+//     themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+// }
+
+
+
 const themeBtn = document.querySelector(".theme-btn");
-themeBtn.addEventListener("click", ()=> {
+
+themeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
     themeBtn.classList.toggle("sun");
+
     localStorage.setItem("saved-theme", getCurrentTheme());
     localStorage.setItem("saved-icon", getCurrentIcon());
 });
 
-const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
 const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
 
 const savedTheme = localStorage.getItem("saved-theme");
 const savedIcon = localStorage.getItem("saved-icon");
 
-if (savedTheme) {
+if(savedTheme){
     document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
     themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
 }
