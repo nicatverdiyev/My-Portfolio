bb1476086bdde684b952395ee43f97609c3afba0
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-02-01 18:18:22 +0400

    scroll finished

--- src/js/main.js
+++ src/js/main.js
@@ -36,4 +36,9 @@ const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 window.addEventListener("scroll",function () {
     scrollTopBtn.classList.toggle("active",window.scrollY > 500);
 });
+
+scrollTopBtn.addEventListener("click",()=> {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
 // -----SCROLL-TO-TOP---BUTTON---END---
